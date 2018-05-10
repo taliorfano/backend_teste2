fe7b0074023181b0b87a86e9d2ab4bc65e1204b3
Talita Orfanó <taliorfano@yahoo.com.br>
2018-05-10 20:41:59 -0300

    Configurações iniciais do projeto

--- SambaTechProjeto/src/main/java/com/sambatech/projeto/SambaTechProjetoApplication.java
+++ SambaTechProjeto/src/main/java/com/sambatech/projeto/SambaTechProjetoApplication.java
@@ -3,6 +3,12 @@ package com.sambatech.projeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/***
+ * Processo Seletivo Samba Tech
+ * Aplicacao que possibilita a conversao de videos para um formato compativel com os padroes Web
+ * @author Talita
+ *
+ */
 @SpringBootApplication
 public class SambaTechProjetoApplication {
 
--- SambaTechProjeto/src/main/java/com/sambatech/projeto/controller/VideoController.java
+++ SambaTechProjeto/src/main/java/com/sambatech/projeto/controller/VideoController.java
@@ -2,11 +2,21 @@ package com.sambatech.projeto.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/****
+ * Controller de videos armazenados
+ * @author Talita
+ *
+ */
 @Controller
 public class VideoController {
 
 	@RequestMapping("/videos")
 	public String listar() {
 		return "ListaVideos";
+	}
+	
+	@RequestMapping("/upload")
+	public String upload() {
+		return "UploadVideo";
 	}
 }
